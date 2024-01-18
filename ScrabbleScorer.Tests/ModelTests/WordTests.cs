d7efae16e4f462094f991b0f6bea4b8361359fee
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-01-18 09:41:47 -0800

    first test passing
    Co-authored-by: Monica Barboza <MonBoza@gmail.com>

--- ScrabbleScorer.Tests/ModelTests/WordTests.cs
+++ ScrabbleScorer.Tests/ModelTests/WordTests.cs
@@ -1,8 +1,21 @@
-namespace ScrabbleScorer.Tests;
+namespace ScrabbleScorer.Models;
 
 [TestClass]
-public class UnitTest1
+public class WordTest
 {
     [TestMethod]
-    public void TestMethod1() { }
+    public void WordConstruct_CreatesInstaceOfWord_Word()
+    {
+        Word newWord = new Word();
+        Assert.AreEqual(typeof(Word), newWord.GetType());
+    }
+
+    [TestMethod]
+    public void WordConstruct_GetWord_String() 
+    { 
+        string gorg = "gorgonzola"
+        Word newWord = new(gorg);
+        Assert.AreEqual(gorg, newWord.Played)
+
+    }
 }
